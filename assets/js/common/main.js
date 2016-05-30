a681e9036382cf63c26d2406454ddc2c84f28c8a
Ihor Orlovskyi <ihororlovskyi@gmail.com>
2016-05-31 00:12:52 +0300

    hello bbq :octocat:

--- assets/js/common/main.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-
-    $('.js-sent-status').click(function(){
-        $(this).hide(100);
-        $('.js-show-status').show(100);
-    });
-
-    //$('.banner-games__img').hide();
-});
--- assets/js/common/scroll_sidebar.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function () {
-    if(window.innerWidth >= 1054) {
-        $(".sidebar").stick_in_parent(
-            { offset_top: 80,
-            inner_scrolling: false }
-        );
-    }
-});
